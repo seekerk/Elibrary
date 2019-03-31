38c0c6c951db27122755edb89d09924cbde5d840
Dan <kek-darlov@yandex.ru>
2019-03-31 19:18:45 +0300

    fix micro error (trying to run on heroku)

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 
 require __DIR__ . "/vendor/autoload.php";
-require __DIR__ . "/vendor/simple_html_dom.php";
 
 $client = new MongoDB\Client(
     'mongodb+srv://admin:elibrary1234@cluster0-sndra.mongodb.net/test?retryWrites=true');
